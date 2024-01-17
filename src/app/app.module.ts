1614fca750c797621ecb35eef8cc3de545d14275
rainiery valerio <rainiery.valerio@economia.gob.do>
2024-01-17 09:58:08 -0400

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MapComponent } from './components/map/map.component';
 
 @NgModule({
   declarations: [
+    MapComponent,
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
