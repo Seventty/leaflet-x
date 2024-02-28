b9e6d28daeb9daaf64440d3c59c443969d6301a1
rainiery valerio <rainiery.valerio@economia.gob.do>
2024-02-28 13:29:54 -0400

    feat: Geoman co dependency added to leaflet map base and a custom marker

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
+import "@geoman-io/leaflet-geoman-free";
 
 @Component({
   selector: 'app-map',
@@ -7,22 +8,86 @@ import * as L from 'leaflet';
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
-  private map: any;
+  private map?: L.Map;
+  private defaultMapLocation: L.LatLngExpression = [19.026319, -70.147792]
+  private defaultZoomLevel: number = 8;
+  private defaultMaxZoom: number = 18
+  private defaultMinZoom: number = 3
 
   private initMap(): void {
     this.map = L.map('map', {
-      center: [ 39.8282, -98.5795 ],
-      zoom: 3
+      center: this.defaultMapLocation,
+      zoom: this.defaultZoomLevel,
+      zoomControl: false,
     });
 
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 18,
-      minZoom: 3,
+    this.geomanControllers();
+
+    const tiles: L.TileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: this.defaultMaxZoom,
+      minZoom: this.defaultMinZoom,
+      /*
+      *copyright message below
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+       */
     });
 
     tiles.addTo(this.map);
   }
+
+  private geomanControllers() {
+    if(this.map){
+      L.control.zoom({
+        position: "topright",
+        zoomInTitle: 'Acercar',
+        zoomOutTitle: 'Alejar'
+      }).addTo(this.map);
+
+      this.map.pm.addControls({
+        position: 'topright',
+        drawCircle: false,
+        drawCircleMarker: false,
+        drawText: false,
+        drawMarker: false,
+        cutPolygon: false,
+        editControls: true
+      });
+
+      this.map.pm.setLang('es');
+
+      const newMarker: any = this.map.pm.Toolbar.copyDrawControl('drawMarker', { name: "newMarker" })
+      newMarker.drawInstance.setOptions({ markerStyle: { icon: this.iconMarker("#00b8e6") } });
+    }
+  }
+
+  private iconMarker(color: string): L.DivIcon {
+    const markerHtmlStyles = `
+    background: ${color};
+    width: 30px;
+    height: 30px;
+    border-radius: 50% 50% 50% 0;
+    border: 1px solid #fff;
+    position: absolute;
+    transform: rotate(-45deg);
+    left: 50%;
+    top: 50%;
+    margin: -15px 0 0 -15px;`;
+
+    const icon = L.divIcon({
+      className: "my-custom-pin",
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      tooltipAnchor: [16, -28],
+      shadowSize: [41, 41],
+      html: `<span style="${markerHtmlStyles}"/>`
+    });
+
+    L.Marker.prototype.options.icon = icon;
+
+    return icon;
+  }
+
   constructor() { }
 
   ngOnInit() {
