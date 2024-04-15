9e103a088a88a70aa9cfede8f9c788b94e8accd8
rainieryeconomia <rainiery.valerio@economia.gob.do>
2024-04-15 18:20:04 -0400

    hexColor type added

---
+++ src/app/shared/types/hexColor.type.ts
@@ -0,0 +1,10 @@
+
+
+/**
+ * Represents a hexadecimal color in '#RRGGBB' format where RR, GG and BB are components
+ * red, green and blue respectively, each represented by two hexadecimal digits.
+ *
+ * example: #fffff
+ * @type {`#${string}`} HexColor
+*/
+export type HexColorType = `#${string}`;
